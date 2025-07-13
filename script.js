bbb8a8d7f22bc723e3e053eefcf4acaa716640f3
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-07-13 14:21:36 +0330

    js start

---
+++ script.js
@@ -0,0 +1,5 @@
+const startBtn=document.querySelector("#startBtn"),
+endBtn=document.querySelector("#endBtn"),
+prevNext=document.querySelectorAll(".prevNext"),
+numbers=document.querySelectorAll(".link");
+
