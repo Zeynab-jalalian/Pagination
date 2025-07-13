d028a41ec0a2a78c9659f4562d817a76c02d4559
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-07-13 14:34:19 +0330

    js part2

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ endBtn=document.querySelector("#endBtn"),
 prevNext=document.querySelectorAll(".prevNext"),
 numbers=document.querySelectorAll(".link");
 
+let currentStep=0;
+
+numbers.forEach((number,numIndex)=>{
+    number.addEventListener("click",(e)=>{
+        e.preventDefault();
+        currentStep=numIndex;
+        document.querySelector(".active").classList.remove("active");
+        number.classList.add("active");
+    })
+})
+
+prevNext.forEach(button=>{
+    button.addEventListener("click",(e)=>{
+        currentStep+=e.target.id==="next" ? 1 : -1;
+        numbers.forEach((number,numIndex)=>{
+            number.classList.toggle("active",numIndex===currentStep);
+        })
+    })
+})
