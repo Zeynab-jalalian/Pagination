12cd959c7150953b5e1d22c4cb59870b208ad89b
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-07-13 15:29:37 +0330

    fix

--- script.js
+++ script.js
@@ -4,20 +4,14 @@ const startBtn = document.querySelector("#startBtn"),
   numbers = document.querySelectorAll(".link");
 
 let currentStep = 0;
+
 const updateBtn = () => {
-  if (currentStep === 4) {
-    endBtn.disabled = true;
-    prevNext[1].disabled = true;
-  } else if (currentStep === 0) {
-    startBtn.disabled = true;
-    prevNext[0].disabled = true;
-  } else {
-    endBtn.disabled = false;
-    prevNext[1].disabled = false;
-    startBtn.disabled = false;
-    prevNext[0].disabled = false;
-  }
+  startBtn.disabled = currentStep === 0;
+  prevNext[0].disabled = currentStep === 0;
+  endBtn.disabled = currentStep === numbers.length - 1;
+  prevNext[1].disabled = currentStep === numbers.length - 1;
 };
+
 numbers.forEach((number, numIndex) => {
   number.addEventListener("click", (e) => {
     e.preventDefault();
@@ -31,26 +25,22 @@ numbers.forEach((number, numIndex) => {
 prevNext.forEach((button) => {
   button.addEventListener("click", (e) => {
     currentStep += e.target.id === "next" ? 1 : -1;
-    numbers.forEach((number, numIndex) => {
-      number.classList.toggle("active", numIndex === currentStep);
-      updateBtn();
-    });
+    document.querySelector(".active").classList.remove("active");
+    numbers[currentStep].classList.add("active");
+    updateBtn();
   });
 });
 
 startBtn.addEventListener("click", () => {
   document.querySelector(".active").classList.remove("active");
-  numbers[0].classList.add("active");
   currentStep = 0;
+  numbers[currentStep].classList.add("active");
   updateBtn();
-  endBtn.disabled = false;
-  prevNext[1].disabled = false;
 });
+
 endBtn.addEventListener("click", () => {
   document.querySelector(".active").classList.remove("active");
-  numbers[4].classList.add("active");
-  currentStep = 4;
+  currentStep = numbers.length - 1;
+  numbers[currentStep].classList.add("active");
   updateBtn();
-  startBtn.disabled = false;
-  prevNext[0].disabled = false;
 });
